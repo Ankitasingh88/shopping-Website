9ebbc762168ac18b17031fea5dd4545f5efb9673
Ankitasingh88 <ankita.shibu.singh@gmail.com>
2025-04-07 23:21:28 +0200

    add on layout

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom"
 const Layout = ({cartItems}) => {
     return (
         <>
-          <Header />
+          <Header productsInCart={cartItems} />
           <Outlet />
         </>
     )
